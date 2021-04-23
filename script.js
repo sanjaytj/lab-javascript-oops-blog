902c2e658fb2124846a3c32ec7389d6a8846c3a6
SanjayTalya <sanjaytalyaofficial@gmail.com>
2021-04-23 18:03:28 +0530

    yet to add eventListener

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 /* Fill your code*/
+class Blog{
+    constructor(title,detail){
+        this.title = title;
+        this.detail - detail;
+    }
+    addTitle(){
+        var title_card = document.createElement('h1');
+        title_card.setAttribute("id","blog-title");
+        console.log(title_card);
+        document.getElementById('card-text').appendChild(title-card);
+        title_card.innerHTML += this.title;
+    }
+    addDescription(){
+        var description = document.getElementById('p');
+        description.setAttribute("id","blog-description");
+        console.log(description);
+        document.getElementById('card-text').appendChild(description);
+        description.innerHTML += this.detail;
+    }
+}
